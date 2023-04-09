ce0221e2f9249fa301ffff97a95eda3e4f59cb17
Jonathan Zhang <admin@cestlavie.moe>
2023-04-09 13:07:31 +0800

    Update

--- bot.py
+++ bot.py
@@ -31,18 +31,27 @@ conn = psycopg2.connect(
     host=os.environ['DB_HOST'],
     port=os.environ['DB_PORT']
 )
-
-# 创建数据库表
 cur = conn.cursor()
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS rss_items (
-        id SERIAL PRIMARY KEY,
-        title TEXT,
-        link TEXT,
-        published TIMESTAMP
-    );
-""")
-conn.commit()
+
+# 判断数据库是否存在
+cur.execute("SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_name = 'rss_items')")
+if cur.fetchone() is None:
+    print("First run detected, I will initialize the database and I won't send toots.")
+    FirstRUN = True
+    # 创建数据库表
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS rss_items (
+            id SERIAL PRIMARY KEY,
+            title TEXT,
+            link TEXT,
+            published TIMESTAMP
+        );
+    """)
+    conn.commit()
+
+
+
+
 
 # 检查每个RSS源的最新项目并将其插入到数据库中
 for feed_url in RSS_FEEDS:
@@ -60,20 +69,29 @@ for feed_url in RSS_FEEDS:
             if latest_item is None or item.published_parsed > latest_item.published_parsed or cur.fetchone() is None:
                 latest_item = item
                 if cur.fetchone() is None:
-                    print(latest_item.link,end=' ---- ')
-                    print(latest_item.title)
-                    # 发送HTTP POST请求到MASTODON_HOST，请求内容为标题和链接
-                    post_data = {"status": f"{latest_item.title} \n{latest_item.link}"}
-                    result = requests.post(URL,data=post_data)
-                    if result.status_code == 200:
-                        print(f'POSTED: {latest_item.title}')
+                    # print(latest_item.link,end=' ---- ')
+                    # print(latest_item.title)
+                    if not FirstRUN:
+                        # 发送HTTP POST请求到MASTODON_HOST，请求内容为标题和链接
+                        post_data = {"status": f"{latest_item.title} \n{latest_item.link}"}
+                        result = requests.post(URL,data=post_data)
+                        if result.status_code == 200:
+                            print(f'POSTED: {latest_item.title}')
+                            cur.execute("""
+                                    INSERT INTO rss_items (title, link, published)
+                                    VALUES (%s, %s, %s)
+                                """, (latest_item.title, latest_item.link, latest_item.published))
+                            conn.commit()
+                        else:
+                            print(f"POST FAILED: {latest_item.title}")
+                            print(result.text)
+                    elif FirstRUN:
+                        print(f'ADDED TO DATABASE: {latest_item.title}')
                         cur.execute("""
                                 INSERT INTO rss_items (title, link, published)
                                 VALUES (%s, %s, %s)
                             """, (latest_item.title, latest_item.link, latest_item.published))
                         conn.commit()
-                    else:
-                        print(result.text)
                 else:
                     print(f'ALREADY POSTED:{latest_item.title}')
                     continue
@@ -81,20 +99,29 @@ for feed_url in RSS_FEEDS:
             if latest_item is None or item.updated_parsed > latest_item.updated_parsed or cur.fetchone() is None:
                 latest_item = item
                 if cur.fetchone() is None:
-                    print(latest_item.link,end=' ---- ')
-                    print(latest_item.title)
+                    # print(latest_item.link,end=' ---- ')
+                    # print(latest_item.title)
                     post_data = {"status": f"{latest_item.title} \n{latest_item.link}"}
-                    # 发送HTTP POST请求到MASTODON_HOST，请求内容为标题和链接
-                    result = requests.post(URL,data=post_data)
-                    if result.status_code == 200:
-                        print(f'POSTED: {latest_item.title}')
+                    if not FirstRUN:
+                        # 发送HTTP POST请求到MASTODON_HOST，请求内容为标题和链接
+                        result = requests.post(URL,data=post_data)
+                        if result.status_code == 200:
+                            print(f'POSTED: {latest_item.title}')
+                            cur.execute("""
+                                INSERT INTO rss_items (title, link, published)
+                                VALUES (%s, %s, %s)
+                            """, (latest_item.title, latest_item.link, latest_item.updated))
+                            conn.commit()
+                        else:
+                            print(f"POST FAILED: {latest_item.title}")
+                            print(result.text)
+                    elif FirstRUN:
+                        print(f'ADDED TO DATABASE: {latest_item.title}')
                         cur.execute("""
                             INSERT INTO rss_items (title, link, published)
                             VALUES (%s, %s, %s)
-                        """, (latest_item.title, latest_item.link, latest_item.updated))
+                            """, (latest_item.title, latest_item.link, latest_item.updated))
                         conn.commit()
-                    else:
-                        print(result.text)
                 else:
                     print(f'ALREADY POSTED:{latest_item.title}')
                     continue
